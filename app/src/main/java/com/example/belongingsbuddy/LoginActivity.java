56d5066df9f808fbc4b7a548fce6eede4158d5a2
William Creaser <wcreaser@ualberta.ca>
2023-10-30 19:28:59 -0600

    Added Buttons and Input to Login Activity
    Added and styled various buttons and text inputs for login functionality, began applying functionality to the buttons

--- app/src/main/java/com/example/belongingsbuddy/LoginActivity.java
+++ app/src/main/java/com/example/belongingsbuddy/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.belongingsbuddy;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,5 +24,21 @@ public class LoginActivity extends AppCompatActivity {
 
         final Button login_button = findViewById(R.id.login_button);
         final Button sign_up_button = findViewById(R.id.create_button);
+        final EditText username_input = findViewById(R.id.username_input);
+        final EditText password_input = findViewById(R.id.password_input);
+        final Button login_confirm_button = findViewById(R.id.login_confirm);
+        final Button sign_up_confirm_button = findViewById(R.id.sign_up_confirm);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_button.setVisibility(View.GONE);
+                sign_up_button.setVisibility(View.GONE);
+                username_input.setVisibility(View.VISIBLE);
+                password_input.setVisibility(View.VISIBLE);
+                login_confirm_button.setVisibility(View.VISIBLE);
+
+            }
+        });
     }
 }
